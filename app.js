bb7b098119e798a8548ac29135e8d972a9cc4569
TheArcher2002 <115656156+TheArcher2002@users.noreply.github.com>
2023-08-23 14:56:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -131,12 +131,6 @@ app.get("/about",function(req,res){
     res.render("about");
 });
 
-// app.post("/work",function(req,res){
-//     let item = req.body.newItem;
-//     workItems.push(item);
-//     res.redirect("/work");
-// });
-
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
